3cffcd72e2f503a40372f57268a531e2cc45c48c
stradivirus <stradivirus9@gmail.com>
2024-12-11 13:59:52 +0900

    오류 수정 1

--- php/src/api/stream.php
+++ php/src/api/stream.php
@@ -1,14 +1,11 @@
 <?php
 require_once __DIR__ . '/../../vendor/autoload.php';
 
+use App\Controllers\MetricsController;
+
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 header('Connection: keep-alive');
 
-try {
-    $controller = new \App\Controllers\MetricsController();
-    $controller->streamMetrics();
-} catch (\Exception $e) {
-    echo "data: " . json_encode(['error' => $e->getMessage()]) . "\n\n";
-    exit;
-}
+$controller = new MetricsController();
+$controller->streamMetrics();
