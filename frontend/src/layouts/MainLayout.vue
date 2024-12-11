c90b520129d2abbfdc5d1e9ac57c42c64916be4d
stradivirus <stradivirus9@gmail.com>
2024-12-11 10:15:29 +0900

    디자인 수정 3

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="min-h-screen bg-gray-100">
     <main class="py-6">
-      <div class="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="max-w-full mx-auto px-4 sm:px-[5%] lg:px-[8%]">  <!-- 화면 크기별로 다른 여백 -->
         <slot></slot>
       </div>
     </main>
   </div>
- </template>
+</template>
