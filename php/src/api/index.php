e2af1222776849d7512e8455cc32fc774276b335
stradivirus <stradivirus9@gmail.com>
2024-12-11 10:33:16 +0900

    데이터 흐름 변경 오류 수정

--- php/src/api/index.php
+++ php/src/api/index.php
@@ -1,13 +1,22 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 $request = $_SERVER['REQUEST_URI'];
 
+// stream 엔드포인트가 아닐 때만 JSON 헤더 설정
+if (!preg_match('/^\/api\/stream\/metrics$/', $request)) {
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+}
+
 // 라우팅 처리
 switch (true) {
+    case preg_match('/^\/api\/stream\/metrics$/', $request):
+        $controller = new \App\Controllers\MetricsController();
+        $controller->streamMetrics();
+        break;
+
     case preg_match('/^\/api\/containers$/', $request):
         require __DIR__ . '/containers.php';
         break;
@@ -20,11 +29,6 @@ switch (true) {
     case preg_match('/^\/api\/metrics\/([^\/]+)$/', $request, $matches):
         $_GET['metric_type'] = $matches[1];
         require __DIR__ . '/metrics.php';
-        break;
-
-    case preg_match('/^\/api\/stream\/metrics$/', $request):
-        $controller = new \App\Controllers\MetricsController();
-        $controller->streamMetrics();
         break;
     
     default:
